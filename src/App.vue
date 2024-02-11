5b17c8645b6fa5327b8a941d77e04cd9f0c8411c
sarahelshark <sarahelsh183@gmail.com>
2024-02-11 19:54:07 +0100

    initial commit  using api

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      base_api_url:'https://rickandmortyapi.com/api/character',
+    };
+  },
+  mounted() {
+    //console.log(this.base_api_url);
+    axios
+    .get(this.base_api_url)
+    .then((response)=>{
+      console.log(response);
+    })
+    
+  },
+};
+</script>
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
