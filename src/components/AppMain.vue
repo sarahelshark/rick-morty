14f598c730376de290189bf385a03b67070a1fee
sarahelshark <sarahelsh183@gmail.com>
2024-02-11 21:19:33 +0100

    feat add main + component CharacterItem

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,44 +1,57 @@
 <script>
-import axios from "axios";
+import axios from 'axios';
+import CharacterItem from './CharacterItem.vue';
 export default {
-  name: "AppMain",
+  name: 'AppMain',
+  components: {
+    CharacterItem
+  },
   data() {
     return {
-      base_api_url: "https://rickandmortyapi.com/api/character",
+      base_api_url: 'https://rickandmortyapi.com/api/character',
       characters: [],
-      error: false,
-    };
+      error: false
+    }
   },
   mounted() {
-    //console.log(this.base_api_url);
+    console.log(this.base_api_url);
     axios
-      .get(this.base_api_url)
+      .get('https://rickandmortyapi.com/api/character')
       .then((response) => {
-        console.log(response.data); //all data including pagination info
-        console.log(response.data.result); //only characters results
-        console.log(this);
+        console.log(response);
+        console.log(response.data); // All data including pagination info
+        console.log(response.data.results); // Only characters results
+        //console.log(this); 
         this.characters = response.data;
       })
       .catch((error) => {
         console.error(error);
         this.error = error.message;
-      });
-  },
-};
+      })
+  }
+}
 </script>
-
 <template>
   <main>
-    <div
-      v-for="character in characters.results"
-      :key="character.id + '_character'"
-    >
-      {{ character.name }}
-      <img :src="character.image" alt="" />
-      {{ character.species }}
-      {{ character.status }}
+    <div class="container">
+      <div class="filters">
+        <!-- add name filter input -->
+        <input type="text" placeholder="Type a name to search">
+        <!-- add a select status filter -->
+        <select name="status" id="status">
+          <option value="" selected>All</option>
+          <option value="alive">Alive</option>
+          <option value="death">Death</option>
+          <option value="unkown">Unknown</option>
+        </select>
+      </div>
+      <div class="row">
+        <CharacterItem v-for="character in characters.results" :key="character.id + '_character'" :character="character">
+        </CharacterItem>
+      </div>
+      <!-- /.row -->
     </div>
+    <!-- /.container -->
   </main>
 </template>
-
-<style></style>
+<style scoped></style>
