9e22f3524a3a8a16280c0f2e5f047d19f65c031e
sarahelshark <sarahelsh183@gmail.com>
2024-02-11 20:25:41 +0100

    stampo in pagina i personaggi da mia struttura dati importata API

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,31 @@ export default {
   name: "App",
   data() {
     return {
-      base_api_url:'https://rickandmortyapi.com/api/character',
-      characters: null
+      base_api_url: "https://rickandmortyapi.com/api/character",
+      characters: null,
+      error: false,
     };
   },
   mounted() {
     //console.log(this.base_api_url);
     axios
-    .get(this.base_api_url)
-    .then((response)=>{
-      console.log(response.data);//all data including pagination info
-      console.log(response.data.result);//only characters results
-      console.log(this);
-      this.characters = response.data;
-    })
-    .catch((error) => {
-       console.error(error);
-    })
-    
+      .get(this.base_api_url)
+      .then((response) => {
+        console.log(response.data); //all data including pagination info
+        console.log(response.data.result); //only characters results
+        console.log(this);
+        this.characters = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+        this.error = error.message;
+      });
   },
 };
 </script>
 <template>
-  <div></div>
+  <div v-for="character in characters.results">
+  {{ character.name }}</div>
 </template>
 
 <style scoped></style>
